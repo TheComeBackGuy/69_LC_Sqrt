3c67bde36c6ef250df3d8efbfbe734ec2cfbfb4d
TheComeBackGuy <71109038+TheComeBackGuy@users.noreply.github.com>
2020-12-22 07:58:52 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>69. Sqrt(x)</h1>
+<div>
+ LeetCode Problems</div>
+`;
+var mySqrt = function (x) {
+  return Math.floor(Math.sqrt(x));
+};
